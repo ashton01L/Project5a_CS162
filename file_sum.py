9876d71256d94c2e329feb3fa9c8da8a7f00b471
ashton01L <ashtonamazing@gmail.com>
2024-10-30 21:56:01 -0500

    changed date, final version

--- file_sum.py
+++ file_sum.py
@@ -1,6 +1,6 @@
 # Author: Ashton Lee
 # Github User: ashton01L
-# Date: 10/29/2024
+# Date: 10/30/2024
 # Description: Write a function named file_sum that takes as a parameter the name of a text file that contains a list
 # of numbers, one to a line.
 def file_sum(filename):
